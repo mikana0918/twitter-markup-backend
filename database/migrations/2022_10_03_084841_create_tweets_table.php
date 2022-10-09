f04f61611fcc45209fd65ad1ca696a26d7b68648
mikana0918 <shogo.kusuhara@gmail.com>
2022-10-09 19:16:11 +0900

    fix: Add user_id foreign on tweets table

--- database/migrations/2022_10_03_084841_create_tweets_table.php
+++ database/migrations/2022_10_03_084841_create_tweets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained('users');
             $table->string('body', 255);
             $table->timestamps();
             $table->softDeletes($column = 'deleted_at', $precision = 0);
