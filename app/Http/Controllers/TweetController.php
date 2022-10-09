b4f06f15ff6b8ee5d52c548a53aef750bb6f092e
mikana0918 <shogo.kusuhara@gmail.com>
2022-10-09 19:18:56 +0900

    refactor: Apply tweet timeline spec to list tweets endpoint

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\Tweet;
@@ -14,8 +15,22 @@ class TweetController extends Controller
      */
     public function list(Request $request): JsonResponse
     {
+        // TODO: If we need infinite loop, we need to add pagination query
+
+        $followingUserIdList = $this->stubMe()->following()->get()->pluck('id');
+
         return response()->json([
-            'tweets' => Tweet::with(['favorites', 'mentions', 'retweets', 'attachments'])->get()
+            'tweets' => Tweet::whereIn('user_id', $followingUserIdList)->with(['favorites', 'mentions', 'retweets', 'attachments'])->orderByDesc('created_at')->get()
         ]);
+    }
+
+    /**
+     * TODO: implement auth module and get auth user
+     *
+     * @return User
+     */
+    private function stubMe(): User
+    {
+        return User::all()->first();
     }
 }
