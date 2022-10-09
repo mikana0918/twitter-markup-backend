184214b8a271a1c3e270a0551c774bb102cfb7ee
mikana0918 <shogo.kusuhara@gmail.com>
2022-10-09 18:41:43 +0900

    fix: Change to capital C for controller

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\Tweet;
 
-class TweetController extends controller
+class TweetController extends Controller
 {
     /**
      * @param Request $request
